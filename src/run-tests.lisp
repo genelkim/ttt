1a87aaea191a81b2a7189eb6c2dae8cccf7a79e4
Gene Kim <gkim21@cs.rochester.edu>
2020-10-19 17:55:07 -0400

    Added tests with debug messages and removed old unit-test running script.

--- src/run-tests.lisp
+++ src/run-tests.lisp
@@ -1,3 +1,6 @@
 (ql:quickload :ttt/tests)
 (in-package :ttt/tests)
 (run)
+; Run again with debug messages on.
+(setf ttt::*ttt-debug-level* 3)
+(run)
--- src/run-unit-tests.lisp
+++
@@ -1,7 +0,0 @@
-;; Runs the unit tests defined through lisp-unit in test/
-;(load "load")
-(ql:quickload :ttt)
-(loop for test-file in (directory "test/*.lisp")
-      do (load test-file))
-(in-package :ttt)
-(lisp-unit:run-tests)
