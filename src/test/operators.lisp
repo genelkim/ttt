b5e8d590f6a2954e7f3974f8d209c5f7a279ec84
Gene Kim <gkim21@cs.rochester.edu>
2020-10-19 06:59:47 -0400

    A bunch more vertical tests.

--- src/test/operators.lisp
+++ src/test/operators.lisp
@@ -10,7 +10,6 @@
     (assert-true (ttt:match-expr pattern '(X Y Z)))
     (assert-true (ttt:match-expr pattern '((X Y) Z)))
     (assert-true (ttt:match-expr pattern '(((((((((X) Y))))))) Z)))
-    
     (assert-false (ttt:match-expr pattern '(Y)))
     (assert-false (ttt:match-expr pattern '(A (Y X))))
     (assert-false (ttt:match-expr pattern '((Y X) Z)))
@@ -21,7 +20,6 @@
     (assert-true (ttt:match-expr pattern '(B (C D E) F)))
     (assert-true (ttt:match-expr pattern '(A B (C E) F)))
     (assert-true (ttt:match-expr pattern '(A B (C D E))))
-    
     (assert-false (ttt:match-expr pattern '(A B (C D) F)))
     (assert-false (ttt:match-expr pattern '(A B (D E) F)))
     (assert-false (ttt:match-expr pattern '(C D E))))
@@ -30,7 +28,6 @@
     (assert-true (ttt:match-expr pattern '(A B (C D E) F)))
     (assert-true (ttt:match-expr pattern '(B (C D E) F)))
     (assert-true (ttt:match-expr pattern '(A B (C D E))))
-    
     (assert-false (ttt:match-expr pattern '(A B (D C E))))
     (assert-false (ttt:match-expr pattern '(A B (C E) F)))
     (assert-false (ttt:match-expr pattern '(A B (C D) F)))
@@ -42,11 +39,36 @@
     (assert-true (ttt:match-expr pattern '(B (C D E) F)))
     (assert-true (ttt:match-expr pattern '(A B (C D E))))
     (assert-true (ttt:match-expr pattern '(A B (D C E))))
-    
     (assert-false (ttt:match-expr pattern '(A B (C E) F)))
     (assert-false (ttt:match-expr pattern '(A B (C D) F)))
     (assert-false (ttt:match-expr pattern '(A B (D E) F)))
-    (assert-false (ttt:match-expr pattern '(C D E)))))
+    (assert-false (ttt:match-expr pattern '(C D E))))
+
+  (let ((pattern '(^@ ({} (A _*) (_* B)) E)))
+    (assert-true (ttt:match-expr pattern '(A (C E B))))
+    (assert-true (ttt:match-expr pattern '((A E C) B)))
+    (assert-false (ttt:match-expr pattern '(A B (C D E) F)))
+    (assert-false (ttt:match-expr pattern '(B (C D E) F)))
+    (assert-false (ttt:match-expr pattern '(A B (C D E))))
+    (assert-false (ttt:match-expr pattern '(A B (D C E)))))
+  
+  (let ((pattern '(^@ ({} (A _*) (_* B)) E)))
+    (assert-true (ttt:match-expr pattern '(A (C E B))))
+    (assert-true (ttt:match-expr pattern '((A E C) B)))
+    (assert-false (ttt:match-expr pattern '(A B (C D E) F)))
+    (assert-false (ttt:match-expr pattern '(B (C D E) F)))
+    (assert-false (ttt:match-expr pattern '(A B (C D E))))
+    (assert-false (ttt:match-expr pattern '(A B (D C E)))))
+  
+  (let ((pattern '(^@ (<> (A _*) (_* B)) E)))
+    (assert-true (ttt:match-expr pattern '(A (C E B))))
+    (assert-false (ttt:match-expr pattern '((A E C) B)))
+    (assert-false (ttt:match-expr pattern '(A B (C D E) F)))
+    (assert-false (ttt:match-expr pattern '(B (C D E) F)))
+    (assert-false (ttt:match-expr pattern '(A B (C D E))))
+    (assert-false (ttt:match-expr pattern '(A B (D C E)))))
+  )
+    
 
 
 
